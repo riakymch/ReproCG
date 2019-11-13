428e3d991bf84e4fb9a2de3119296dc1ba743cf2
КщÑRoman Iakymchuk <roman.iakymchuk@sorbonne-universite.fr>
2019-11-13 20:06:03 +0100

    Some more cleaning

--- src/loadEnv.sh
+++
@@ -1,2 +0,0 @@
-export PATH=/state/partition1/soft/gnu/gcc-5.3.0/bin:$PATH
-export LD_LIBRARY_PATH=/state/partition1/soft/gnu/gcc-5.3.0/lib64/lib:/state/partition1/soft/intel/compilers_and_libraries_2016.1.150/linux/compiler/lib/intel64_lin/:/state/partition1/soft/intel/composer_xe_2011_sp1/mkl/lib/intel64/:$LD_LIBRARY_PATH
